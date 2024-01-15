296a5fbeb7f35cd210c3a917374d5def15b88f7f
Ольга Ротбардт <knyajna.tarakanova@gmail.com>
2024-01-16 00:38:33 +0400

    Add another sum_of_tuple_elements solution

--- interview/sum_of_tuple_elements.py
+++ interview/sum_of_tuple_elements.py
@@ -2,25 +2,41 @@ def func(my_tuple):
     result = 0
     for i in range(0, len(my_tuple)):
         if type(my_tuple[i]) is str:
-            print(f"Индекс элемента, являющегося строкой: {i}")
             return i
         else:
-            result += + my_tuple[i]
-    print(f"Сумма элементов равна {result}")
+            result += my_tuple[i]
+    return result
 
 
-# альтернативный вариант:
+print(func((1, 2, 3, 4, 5)))
+print(func((1, 2, 3.0, 4, 5)))
+print(func((1, 2, "a", 4, 5)))
+
+
+# 1 альтернативный вариант:
+# def func(my_tuple):
+#     result = 0
+#     for i in range(0, len(my_tuple)):
+#         if type(my_tuple[i]) is str:
+#             print(f"Индекс элемента, являющегося строкой: {i}")
+#             return
+#         else:
+#             result += my_tuple[i]
+#     print(f"Сумма элементов равна {result}")
+
+
+# 2 альтернативный вариант:
 # def func(my_tuple):
 #     result = 0
 #     for i, item in enumerate(my_tuple):
 #         if type(item) is str:
 #             print(f"Индекс элемента, являющегося строкой: {i}")
-#             return i
+#             return
 #         elif isinstance(item, (int, float)):
 #             result += item
 #     print(f"Сумма элементов равна {result}")
 #     return result
 
-func((1, 2, 3, 4, 5))
-func((1, 2, 3.0, 4, 5))
-func((1, 2, "a", 4, 5))
+# func((1, 2, 3, 4, 5))
+# func((1, 2, 3.0, 4, 5))
+# func((1, 2, "a", 4, 5))
